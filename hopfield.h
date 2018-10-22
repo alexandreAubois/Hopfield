b88bdae415c135074b14414bf938457d054122f7
Unknown <aubois.alexandre@gmail.com>
2018-10-22 09:13:38 +0200

    Initialisation des fonctions dans le .h
    Définition des fonctions suivante  dans le .h :
    affiche_reseau
    convertion_binaire
    calcul_poids
    set_entree
    get_sortie
    iteration_suivante

--- hopfield.h
+++ hopfield.h
@@ -9,11 +9,11 @@ typedef struct Entree
 	int largeur_image;
 	int hauteur_image;
 	int nombre_motif;
-	
+
 } entree;
 
 typedef struct Reseau
-{		
+{
 		struct Entree * entree;
 		int ** poids;
 		int ** sortie;
@@ -24,3 +24,15 @@ typedef struct Reseau
 int initialise_entree(Entree * entree, int nombre_motifs);
 
 int initialise_reseau(Reseau * reseau, Entree * entree);
+
+void affiche_reseau(Reseau * reseau);
+
+void convertion_binaire(Reseau * reseau);
+
+void calcul_poids(Reseau * reseau);
+
+void set_entree(Reseau * reseau, Entree * entree);
+
+void get_sortie(Reseau * reseau, int * sortie);
+
+void iteration_suivante(Reseau * reseau, int indice);
