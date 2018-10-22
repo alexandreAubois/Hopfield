20ba24aca9355a10488b6ce54a03808aea3aebaa
Unknown <aubois.alexandre@gmail.com>
2018-10-22 17:40:28 +0200

    Corrections d'erreurs

--- main.c
+++ main.c
@@ -4,15 +4,19 @@
 int main(void)
 {
 	srand((unsigned int) time(NULL));
-	
+
 	Entree * entree = NULL;
-	
+
+	entree = (Entree*) malloc(sizeof(Entree));
+
 	initialise_entree(entree, NB_MOTIF);
 
 	conversion_binaire(entree);
-	
+
 	Reseau * reseau = NULL;
-	
+
+	reseau = (Reseau *) malloc(sizeof(Reseau));
+
 	initialise_reseau(reseau, entree);
 
 	calcul_noeud(reseau);
