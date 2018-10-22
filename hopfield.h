1c30c272a6d5d5d71ab1722ceddd3ca3765656f7
Desrumaux <guillaume.desrumaux@isen.yncrea.fr>
2018-10-22 08:37:52 +0200

    Types basiques

--- hopfield.h
+++ hopfield.h
@@ -6,11 +6,21 @@
 typedef struct Entree
 {
 	int ** motif;
-
 	int largeur_image;
 	int hauteur_image;
+	int nombre_motif;
+	
 } entree;
 
+typedef struct Reseau
+{		
+		struct Entree * entree;
+		int ** poids;
+		int ** sortie;
+		int nombreNeurone;
+		int * etatNeurones;
+} reseau;
 
-int initialise_entree(Entree entree);
+int initialise_entree(Entree * entree, int nombre_motifs);
 
+int initialise_reseau(Reseau * reseau, Entree * entree);
